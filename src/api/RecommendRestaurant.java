6e1e64c9b840b07ee0c883b7e7036398ba954a76
SpecialCong <specialcong@gmail.com>
2017-02-14 21:57:39 -0800

    Restaurant Recommendation Application

---
+++ src/api/RecommendRestaurant.java
@@ -0,0 +1,70 @@
+package api;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.json.JSONArray;
+
+import db.DBConnection;
+//import db.MongoDBConnection;
+import db.MySQLDBConnection;
+
+/**
+ * Servlet implementation class RecommendRestaurant
+ */
+@WebServlet("/recommendation")
+public class RecommendRestaurant extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	private static DBConnection connection = new MySQLDBConnection();
+
+	// private static DBConnection connection = new MongoDBConnection();
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public RecommendRestaurant() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// allow access only if session exists
+		HttpSession session = request.getSession();
+		if (session.getAttribute("user") == null) {
+			response.setStatus(403);
+			return;
+		}
+		JSONArray array = null;
+
+		if (request.getParameterMap().containsKey("user_id")) {
+			String userId = request.getParameter("user_id");
+			array = connection.recommendRestaurants(userId);
+		}
+		RpcParser.writeOutput(response, array);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// allow access only if session exists
+		HttpSession session = request.getSession();
+		if (session.getAttribute("user") == null) {
+			response.setStatus(403);
+			return;
+		}
+		doGet(request, response);
+	}
+
+}
---
+++ src/db/DBOperationResult.java
@@ -0,0 +1,5 @@
+package db;
+
+public enum DBOperationResult {
+	SUCCESS, SQL_CONFIG_ERROR, SQL_QUERY_ERROR
+}
